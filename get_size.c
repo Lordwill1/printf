9cd4396f11c0d8aaab710251d9ec44c39a0b78ae
Godswill Kalu <messagelordwill@gmail.com>
2022-10-19 13:49:10 +0100

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
+ * get_size -Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
